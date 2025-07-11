fac98c6fab4f877df1308bc8923341b3397e780f
Phineas1500 <41450967+Phineas1500@users.noreply.github.com>
2025-07-11 13:26:33 -0400

    64 bit exits with 0 but crashes

--- main.c
+++ main.c
@@ -6,18 +6,21 @@
 #include "xX_main_Xx.h"
 
 int main(int argc, char *const argv[]) {
-    // printf("main: ENTRY with argc=%d\n", argc);
+    printf("main: ENTRY with argc=%d\n", argc);
     char envp[100] = {0};
     if (getenv("TERM")) {
         const char *term = getenv("TERM");
         snprintf(envp, sizeof(envp), "TERM=%s", term);
     }
+    printf("main: calling xX_main_Xx\n");
     int err = xX_main_Xx(argc, argv, envp);
     if (err < 0) {
         fprintf(stderr, "xX_main_Xx: %s\n", strerror(-err));
         return err;
     }
+    printf("main: xX_main_Xx succeeded, mounting filesystems\n");
     do_mount(&procfs, "proc", "/proc", "", 0);
     do_mount(&devptsfs, "devpts", "/dev/pts", "", 0);
+    printf("main: calling task_run_current()\n");
     task_run_current();
 }
